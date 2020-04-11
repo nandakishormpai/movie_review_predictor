6585f337b9504076efc2196a32f7c149d1655701
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-04-11 13:28:28 +0530

    now ready

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # learn_through_issues
-we raise and solve issues
+Review analysis model
+
+train.py which is used to train the model is made through the source i got from https://machinelearningmastery.com/develop-word-embedding-model-predicting-movie-review-sentiment/
+
+i worked on prediction script
+
+first clone it and what you have to do is paste your review in the text file my_review inside the file my_prediction 
+
+then first run train.py
+
+then run predict.py
+
+i hope you get your result
+
+
+
+
+		   
---
+++ predict.py
@@ -0,0 +1,187 @@
+import numpy as np
+import tensorflow.python.keras
+from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Conv1D
+from tensorflow.python.keras.layers import Flatten
+from tensorflow.python.keras.layers import MaxPooling1D
+from tensorflow.python.keras.layers.embeddings import Embedding
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+import tensorflow.python.keras
+from string import punctuation
+from os import listdir
+from collections import Counter
+from nltk.corpus import stopwords
+# load and evaluate a saved model
+from numpy import loadtxt
+from tensorflow.python.keras.models import load_model
+
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# turn a doc into clean tokens
+def clean_doc(doc):
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# remove remaining tokens that are not alphabetic
+	tokens = [word for word in tokens if word.isalpha()]
+	# filter out stop words
+	stop_words = set(stopwords.words('english'))
+	tokens = [w for w in tokens if not w in stop_words]
+	# filter out short tokens
+	tokens = [word for word in tokens if len(word) > 1]
+	return tokens
+
+# load doc and add to vocab
+def add_doc_to_vocab(filename, vocab):
+	# load doc
+	doc = load_doc(filename)
+	# clean doc
+	tokens = clean_doc(doc)
+	# update counts
+	vocab.update(tokens)
+
+# load all docs in a directory
+def process_docs(directory, vocab, is_trian):
+	# walk through all files in the folder
+	for filename in listdir(directory):
+		# skip any reviews in the test set
+		if is_trian and filename.startswith('cv9'):
+			continue
+		if not is_trian and not filename.startswith('cv9'):
+			continue
+		# create the full path of the file to open
+		path = directory + '/' + filename
+		# add doc to vocab
+		add_doc_to_vocab(path, vocab)
+
+# define vocab
+vocab = Counter()
+# add all docs to vocab
+process_docs('txt_sentoken/neg', vocab, True)
+process_docs('txt_sentoken/pos', vocab, True)
+
+
+
+min_occurane = 2
+tokens = [k for k,c in vocab.items() if c >= min_occurane]
+
+
+# save list to file
+def save_list(lines, filename):
+	# convert lines to a single blob of text
+	data = '\n'.join(lines)
+	# open file
+	file = open(filename, 'w')
+	# write text
+	file.write(data)
+	# close file
+	file.close()
+
+# save tokens to a vocabulary file
+save_list(tokens, 'vocab.txt')
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# load the vocabulary
+vocab_filename = 'vocab.txt'
+vocab = load_doc(vocab_filename)
+vocab = vocab.split()
+vocab = set(vocab)
+
+# turn a doc into clean tokens
+def clean_doc(doc, vocab):
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# filter out tokens not in vocab
+	tokens = [w for w in tokens if w in vocab]
+	tokens = ' '.join(tokens)
+	return tokens
+
+	# load all docs in a directory
+def process_docs(directory, vocab, is_trian):
+	documents = list()
+	# walk through all files in the folder
+	for filename in listdir(directory):
+		# skip any reviews in the test set
+		if is_trian and filename.startswith('cv9'):
+			continue
+		if not is_trian and not filename.startswith('cv9'):
+			continue
+		# create the full path of the file to open
+		path = directory + '/' + filename
+		# load the doc
+		doc = load_doc(path)
+		# clean doc
+		tokens = clean_doc(doc, vocab)
+		# add to list
+		documents.append(tokens)
+	return documents
+
+# load all training reviews
+positive_docs = process_docs('txt_sentoken/pos', vocab, True)
+negative_docs = process_docs('txt_sentoken/neg', vocab, True)
+train_docs = negative_docs + positive_docs
+
+
+
+# create the tokenizer
+tokenizer = Tokenizer()
+# fit the tokenizer on the documents
+tokenizer.fit_on_texts(train_docs)
+
+
+
+	# load all docs in a directory
+def process_docs(vocab):
+	documents = list()
+	doc = load_doc("my_prediction/my_review.txt")
+	# clean doc
+	tokens = clean_doc(doc, vocab)
+	# add to list
+	documents.append(tokens)
+	return documents
+
+predict_docs = process_docs(vocab)
+
+
+encoded_docs = tokenizer.texts_to_sequences(predict_docs)
+
+X = pad_sequences(encoded_docs, maxlen=1317, padding='post')
+
+
+# load model
+model = load_model('my_model.h5')
+
+
+y=model.predict_classes(np.array(X))
+
+
+if (y == [[1]]) :
+	print("\n good movie \n")
+else :
+	print("\n not recommended for humans \n")
+
